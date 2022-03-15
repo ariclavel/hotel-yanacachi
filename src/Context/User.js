aef6668c819a1bd1f3b83b96abc3136e8697fe9d
ariclavel <a01706629@itesm.mx>
2022-03-15 01:10:40 -0400

    user context

---
+++ src/Context/User.js
@@ -0,0 +1,12 @@
+import {useState, createContext} from "react";
+
+export const userContext = createContext({
+    currentUser: null,
+    setCurrentUser: () => null
+
+});
+export const userProvider = ({children} ) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const value = {currentUser, setCurrentUser};
+    return <userContext.Provider value= {value}> {children} </userContext.Provider>
+}
